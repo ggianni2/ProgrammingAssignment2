dc562fd7f7b3cf3ef6a5964e838f8dcaec318d78
ggianni2 <67925261+ggianni2@users.noreply.github.com>
2020-07-21 10:26:58 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function calls a matrix which can be inverted and cached
 
-## Write a short comment describing this function
-
+## x is a square invertable matrix. it is followed by a list of functions to set and get the matrix and its inverted table.
+## when it has not been calculated the output will display "NULL". When it has, the output will be cached so it can be accessed by the next step. 
 makeCacheMatrix <- function(x = matrix()) {
+     inv<- NULL
+     set<- function(y){
+          x<<-y
+          inv<<-NULL
+     }
+     get<- function(){x}
+     setInverse<-function(inverse){inv<<-inverse}
+     getInverse<- function() inv
+     list(set = set, get = get, setInverse= setInverse, getInverse= getInverse)
 
 }
-
-
-## Write a short comment describing this function
+## this function checks the cache of the first. If it finds the inverse is not NULL, it returns the inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+     inv<- x$getInverse()
+     if(!is.null(inv)){
+          return(inv)
+     }
+## If the inverse has not been calculated, it finds NULL and calculates the inverse, sets it in the cache, and returns it.
+     mat <- x$get()
+     inv<- solve(mat, ...)
+     x$setInverse(inv)
+     return(inv)
 }
